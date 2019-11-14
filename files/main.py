e7d480c91d069a692cd905ad4fb9b986418f7e5c
Bernhard Bablok <bablokb@gmx.de>
2019-11-14 10:22:15 +0100

    added set_time() and get_time() methods

--- files/main.py
+++ files/main.py
@@ -14,6 +14,7 @@ import board
 import busio
 from digitalio import DigitalInOut
 import time
+import rtc
 import neopixel
 import displayio
 from adafruit_bitmap_font import bitmap_font
@@ -21,6 +22,17 @@ from adafruit_display_text import label
 from adafruit_esp32spi import adafruit_esp32spi, adafruit_esp32spi_wifimanager
 
 from secrets import secrets  # file secrets.py
+
+# --- constants   -----------------------------------------------------
+
+WDAY={0:'Mon',
+      1:'Die',
+      2:'Mit',
+      3:'Don',
+      4:'Fre',
+      5:'Sam',
+      6:'Son'
+  }
 
 # ---------------------------------------------------------------------
 def get_wifi(secrets):
@@ -54,6 +66,21 @@ def set_text(group,text,font,color):
   text_area.y = -y
   group.append(text_area)
 
+# ---------------------------------------------------------------------
+def set_time(con,tz):
+  response = con.get("http://worldtimeapi.org/api/timezone/"+tz)
+  tinfo    = response.json()
+  print(tinfo['datetime'])
+  rtc.RTC().datetime = time.localtime(tinfo['unixtime']+tinfo['raw_offset'])
+  response.close()
+
+# ---------------------------------------------------------------------
+def get_time():
+  now = rtc.RTC().datetime
+  return ("%s, %02d.%02d.%02d %02d:%02d" %
+          (WDAY[now.tm_wday],now.tm_mday,now.tm_mon,now.tm_year,
+           now.tm_hour,now.tm_min))
+
 # --- main-loop   -----------------------------------------------------
 
 display = board.DISPLAY
@@ -62,26 +89,30 @@ FONT    = bitmap_font.load_font("/DroidSansMono-16.bdf")
 COLOR   = 0xFFFFFF
 
 # set background
+print("\nloading background")
 group.append(get_background("wolken.bmp"))
 
-textbox = set_text(group,"connecting to AP ...",FONT,COLOR)
-
-# Show it
-display.show(group)
-
 # connect to internet
+print("connecting to AP ...")
 connection = get_wifi(secrets)
+
+# set local time
+print("get local time from worldtimeapi.org")
+set_time(connection,secrets['timezone'])
+print("local time is: %s" % get_time())
 
 weather_data = "no data available yet"
 
 while True:
   try:
-    set_text(group,"connecting to https://wttr.in",FONT,COLOR)
-    display.show(group)
+    print("connecting to https://wttr.in")
     response     = connection.get("https://wttr.in/München?AT0")
     weather_data = response.text
   except:
-    pass           # keep old data
+    print("wttr.in error: code: %d, reason: %s" %
+          (response.status_code,response.reason))
+  finally:
+    response.close()
 
   set_text(group,weather_data,FONT,COLOR)
   display.show(group)
